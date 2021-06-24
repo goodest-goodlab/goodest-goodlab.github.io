d4482ddc0948c05ecea060cfc0eb5585bfaa3c92
goodest-goodlab <gregg.thomas@umontana.edu>
2021-06-24 17:54:46 -0600

    added phyllotis sampling multiqc reports

--- scripts/generators/phyllotis_generator.py
+++ scripts/generators/phyllotis_generator.py
@@ -32,6 +32,7 @@ html_template = """
     <div class="row" id="link-row">
     <div class="col-6-24" id="margin"></div>
     <div class="col-12-24" id="link-col">
+        <h2>Genome assembly</h2>
         <ul>
             <li><h4><a href="UNI1803_hifiasm_hirise_report.html">hifiasm hirise assembly report</a></h4></li>
             <li><h4><a href="hifiasm_tad_report.html">hifiasm TAD report</a></h4></li>
@@ -40,6 +41,17 @@ html_template = """
             <li><h4><a href="DTG-OmniC-263_R1_001_fastqc.html">OmniC read pair 1 FastQC report</a></h4></li>
             <li><h4><a href="DTG-OmniC-263_R2_001_fastqc.html">OmniC read pair 2 FastQC report</a></h4></li>
             <li><h4><a href="annotation.html">Annotation stats</a></h4></li>
+        </ul>
+    </div>
+    <div class="col-6-24" id="margin"></div>
+    </div>
+
+    <div class="col-6-24" id="margin"></div>
+    <div class="col-12-24" id="link-col">
+        <h2>Phyllotis population samples</h2>
+        <ul>
+            <li><h4><a href="multiqc_report_trimming.html">Read quality</a></h4></li>
+            <li><h4><a href="multiqc_report_raw_bams.html">Mapping report</a></h4></li>
         </ul>
     </div>
     <div class="col-6-24" id="margin"></div>
